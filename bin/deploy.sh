3ba8ba5d95b6643ed8a54c90d19a9680dfdc95a8
Nicolas Sauveur <nicolas.sauveur@gmail.com>
2025-06-04 14:54:08 +0200

    Revert previous commit

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -61,13 +61,9 @@ fi
 # live ==> no dev dependencies
 # staging ==> all dependencies - since we load DoctrineFixturesBundle and WebProfilerBundle in bundles.php
 case $TRACK in
-    bkup)
+    live)
     BUILD_APP_ENV=prod
     COMPOSER_INSTALL_ARGS="--no-scripts --no-dev"
-    ;;
-    live)
-    BUILD_APP_ENV=dev
-    COMPOSER_INSTALL_ARGS="--no-scripts"
     ;;
     staging)
     BUILD_APP_ENV=dev
