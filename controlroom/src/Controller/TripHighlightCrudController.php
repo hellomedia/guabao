28fc66a519b7e9972037f684ac503a4d7fa04ac3
Nicolas Sauveur <nicolas.sauveur@gmail.com>
2025-08-12 21:07:18 +0200

    Admin: fix trip select items sort order

--- controlroom/src/Controller/TripHighlightCrudController.php
+++ controlroom/src/Controller/TripHighlightCrudController.php
@@ -2,7 +2,9 @@
 
 namespace Controlroom\Controller;
 
+use App\Entity\Trip;
 use App\Entity\TripHighlight;
+use Doctrine\ORM\EntityRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
@@ -29,7 +31,19 @@ class TripHighlightCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        yield AssociationField::new('trip');
+        yield AssociationField::new('trip')
+            ->setFormTypeOptions([
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('t')
+                        ->orderBy('t.startedAt', 'DESC'); // change to the field you want
+                },
+                'choice_label' => function (Trip $trip): string {
+                    if ($trip->hasParentTrip()) {
+                        return $trip->getParentTrip()->getShortNameWithFallback() . ' ' . $trip->getName();
+                    }
+                    return $trip->getName();
+                }
+            ]);
         
         yield TextField::new('nameEn', 'Name EN');
         yield TextField::new('nameFr', 'Name FR');
