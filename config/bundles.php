5cc146956629d5a20e0d17b6e34053bbc7676461
Nicolas Sauveur <nicolas.sauveur@gmail.com>
2025-08-14 23:07:53 +0200

    Add symfony UX Chartjs bundle

--- config/bundles.php
+++ config/bundles.php
@@ -29,4 +29,5 @@ return [
     Sentry\SentryBundle\SentryBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
+    Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
 ];
