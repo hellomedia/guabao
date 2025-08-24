9f3718e44c8482e626f9e59ac79f12e8e4243cd3
Nicolas Sauveur <nicolas.sauveur@gmail.com>
2025-08-24 10:54:35 +0200

    Place: make toString always return a string

--- src/Entity/Place.php
+++ src/Entity/Place.php
@@ -62,7 +62,7 @@ class Place implements EntityInterface
 
     public function __toString()
     {
-        return $this->name;
+        return $this->name ?? ($this->latitude . ' / ' . $this->longitude);
     }
 
     public function getId(): int
