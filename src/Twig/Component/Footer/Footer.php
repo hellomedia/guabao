c9bde77e45222004ac4a5a526b4a6ed2b8883ef1
Nicolas Sauveur <nicolas.sauveur@gmail.com>
2025-09-24 01:04:52 +0200

    Revert twig component template location for now
    When placed in Twig/Component directory, tailwind does not scan the 
    templates for tailwind classes

--- src/Twig/Component/Footer/Footer.php
+++ src/Twig/Component/Footer/Footer.php
@@ -5,7 +5,7 @@ namespace App\Twig\Component\Footer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
-#[AsTwigComponent(name: 'footer', template: '@component/Footer/footer.html.twig')]
+#[AsTwigComponent(name: 'footer', template: 'component/footer.html.twig')]
 class Footer extends AbstractController
 {
 
