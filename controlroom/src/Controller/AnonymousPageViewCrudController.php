a2211bbe897c53b4beb767599a3205f088c6fc59
Nicolas Sauveur <nicolas.sauveur@gmail.com>
2026-04-06 07:48:35 +0200

    Admin: improve page view filters

--- controlroom/src/Controller/AnonymousPageViewCrudController.php
+++ controlroom/src/Controller/AnonymousPageViewCrudController.php
@@ -3,6 +3,7 @@
 namespace Controlroom\Controller;
 
 use App\Entity\Stats\AnonymousPageView;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
@@ -16,6 +17,17 @@ class AnonymousPageViewCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return AnonymousPageView::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return parent::configureCrud($crud)
+            ->setSearchFields([
+                'visit.id',
+                'visit.visitor.id',
+                'visit.visitor.alias',
+            ])
+        ;
     }
 
     public function configureFilters(Filters $filters): Filters
